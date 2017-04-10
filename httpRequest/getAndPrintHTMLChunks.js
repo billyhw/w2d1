94155006ee8c0ea45acb224daf97cf72a6dbaf27
Billy Chang <billyheungwing@yahoo.ca>
2017-04-10 16:23:13 +0000

    added getAndPrintHTML

---
+++ httpRequest/getAndPrintHTMLChunks.js
@@ -0,0 +1,47 @@
+function getAndPrintHTMLChunks () {
+
+  var https = require('https');
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-example/step1.html'
+  };
+
+  // notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+  https.get(requestOptions, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    var numChunks = 0;
+    response.on('data', function (data) {
+      numChunks += 1;
+      console.log("line: ", numChunks, ":", data, "\n");
+    });
+
+
+    // the callback is involved if error found
+    response.on('end', function () {
+      console.log('Response stream complete. Total ', numChunks, ' chunks received.');
+      console.log('There are ', numError, ' errors found during the streaming.');
+      });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    var numError = 0;
+    response.on('error', function () {
+      numError +=  1;
+      console.log('Error Received. Please be careful');
+    });
+
+    // Note that this is unnecessary, this will not be output.
+    // response.on('finish', function () {
+    //   console.log('Finish flushing data to system.');
+    // })
+
+  });
+}
+
+getAndPrintHTMLChunks()
